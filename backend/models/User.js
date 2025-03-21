3ee5c5db67f23fd95b625b93f486c9ae1de0d823
annuu1 <mannusng876@gmail.com>
2025-03-21 19:19:04 +0530

    .

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,19 +2,13 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  firstName:{type:String, required:true},
+  lastName:{type:String, required:true},
+  // username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, unique: true, lowercase: true, trim: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-});
-
-// Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
