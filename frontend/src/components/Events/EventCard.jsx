5594f227ab8ce42fb041f0ded9a2bdbd692b48dc
annuu1 <mannusng876@gmail.com>
2025-03-23 16:31:34 +0530

    updated the event card

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -1,36 +1,36 @@
-import React, {useState} from 'react'
-import styles from '../../styles/EventCard.module.css'
+import React, { useState } from "react";
+import styles from "../../styles/EventCard.module.css";
 
-import toggleOn from '../../assets/icons/toggleOn.svg'
-import toggleOff from '../../assets/icons/toggleOff.svg'
-import copyBtn from '../../assets/icons/copyBtn.svg'
-import deleteBtn from '../../assets/icons/deleteBtn.svg'
+import toggleOn from "../../assets/icons/toggleOn.svg";
+import toggleOff from "../../assets/icons/toggleOff.svg";
+import copyBtn from "../../assets/icons/copyBtn.svg";
+import deleteBtn from "../../assets/icons/deleteBtn.svg";
 
-function EventCard({title, date, time, duration}) {
-    
+function EventCard({ title, date, time, description, isActive }) {
   return (
-
-    <div className={styles.card}> 
-    <div className={styles.line}></div>
-    <div className={styles.content}>
-        <div>
+    <div className={styles.card}>
+      <div className={styles.line}></div>
+      <div className={styles.content}>
+        <div className={styles.cardHeader}>
           <h2 className={styles.title}>{title}</h2>
+          <span className={styles.editIcon}>✏️</span>
         </div>
-        <div>
-          <h4 className={styles.date}>{date}</h4>
-          <h4 className={styles.time}>{time}</h4>
-          <h4 className={styles.duration}>{duration}</h4>
-        </div>
+        <p className={styles.cardDetails}>
+        {date}
+        <br />
+        {time}
+        <br />
+        {description}
+      </p>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.cardFooter}>
+        <img src={toggleOn} alt="" className={styles.action} />
+        <span className={styles.icon}>📋</span>
+        <img src={deleteBtn} alt="" className={styles.action} />
+      </div>
     </div>
-    <hr className={styles.divider}/>
-    <div className={styles.actions}>
-    <img src={toggleOn} alt=""  className={styles.action}/>
-    <img src={copyBtn} alt="" className={styles.action} />
-    <img src={deleteBtn} alt="" className={styles.action}/>
-    </div>
-    
-    </div>
-  )
+  );
 }
 
-export default EventCard
+export default EventCard;
