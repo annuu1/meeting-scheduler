b4a1ebefcdb59646f696655aac0cb06a6eade956
annuu1 <mannusng876@gmail.com>
2025-03-25 17:20:44 +0530

    added the retry logic in db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {});
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
+  let retries = 5;
+  while (retries) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI, {});
+      console.log('MongoDB connected');
+      break;
+    } catch (error) {
+      console.error('MongoDB connection error:', error);
+      retries--;
+      if (!retries) process.exit(1);
+      await new Promise(res => setTimeout(res, 5000));
+    }
   }
 };
-
 module.exports = connectDB;
