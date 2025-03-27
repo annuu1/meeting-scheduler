9f4ae71ef287a17f49d90b9815b49e6bfb6e9e02
annuu1 <mannusng876@gmail.com>
2025-03-27 15:05:52 +0530

    prefrerence input styles update

--- frontend/src/components/settings/ProfileForm.jsx
+++ frontend/src/components/settings/ProfileForm.jsx
@@ -1,88 +1,142 @@
-import React, {useState, useEffect} from 'react'
-import styles from '../../styles/ProfileForm.module.css'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/ProfileForm.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function ProfileForm() {
-      const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        email:"",
-        password:"",
-        confirmPassword:"",
-      })
-      const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const navigate = useNavigate();
 
-      useEffect(() => {
-        const fetchUserData = async () => {
-          try {
-            const token = localStorage.getItem('token');
-            const response = await axios.get('http://localhost:5000/api/auth/profile', {
-              headers: {
-                Authorization: `${token}`,
-              },
-            });
-            const { firstName, lastName, email } = response.data;
-            setFormData({ firstName, lastName, email, password: "", confirmPassword: "" });
-          } catch (error) {
-            console.error("Error fetching user data:", error);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "http://localhost:5000/api/auth/profile",
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
           }
-        };
-    
-        fetchUserData();
-      }, []);
-    
-      const handleChange = (e)=>{
-        const {name, value} = e.target;
-        setFormData((prev)=>({...prev, [name]:value}) )
+        );
+        const { firstName, lastName, email } = response.data;
+        setFormData({
+          firstName,
+          lastName,
+          email,
+          password: "",
+          confirmPassword: "",
+        });
+      } catch (error) {
+        console.error("Error fetching user data:", error);
       }
-      const handleSubmit = (e)=>{
-        e.preventDefault();
-        const token = localStorage.getItem('token');
-        const config = {
-          headers:{
-            'Authorization': `${token}`
-          }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    const config = {
+      headers: {
+        Authorization: `${token}`,
+      },
+    };
+
+    axios
+      .put("http://localhost:5000/api/auth/profile", formData, config)
+      .then((response) => {
+        if (response.data.shouldLogout) {
+          localStorage.removeItem("token");
+          navigate("/login");
+        } else {
+          navigate("/dashboard");
         }
-
-        axios.put('http://localhost:5000/api/auth/profile', formData, config)
-        .then((response)=>{
-            if(response.data.shouldLogout){
-              localStorage.removeItem('token');
-              navigate('/login')
-            }else{
-              navigate('/dashboard')
-            }
-        })
-        .catch((err)=>{
-          console.log(err.response.data.error)
-        })
-      }
-    
+      })
+      .catch((err) => {
+        console.log(err.response.data.error);
+      });
+  };
 
   return (
-      <div className={styles.container}>
-            <div className={styles.formContainer}>
-                <div className={styles.formHeader}>
-                    <h1>form header</h1>
-                </div>
-                <form className={styles.form} onSubmit={handleSubmit}>
-                  <label htmlFor ='firstName'>First Name</label>
-                  <input type="text" name='firstName' value={formData.firstName} onChange={handleChange} required/>
-                  <label htmlFor = 'lastName'>Last Name</label>
-                  <input type="text" name='lastName' value={formData.lastName} onChange={handleChange} required/>
-                  <label htmlFor = 'email'>Email</label>
-                  <input type="email" name='email' value={formData.email} onChange={handleChange} required/>
-                  <label htmlFor = 'password'>Password</label>
-                  <input type="password" name='password' value={formData.password} onChange={handleChange} />
-                  <label htmlFor = 'confirmPassword'>Confirm Password</label>
-                  <input type="password" name='confirmPassword' 
-                  value={formData.confirmPassword} onChange={handleChange} />
-                  <button type="submit">Save</button>
-                </form>
-            </div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <h1 className={styles.headerTitle}>Profile</h1>
+          <p className={styles.headerSubtitle}>
+            Manage settings for your profile
+          </p>
+        </div>
+      </div>
+      <div className={styles.formContainer}>
+        <div className={styles.formHeader}>
+          <p className={`${styles.formTitle} ${styles.active}`}>Edit Profile</p>
+        </div>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
           </div>
-    )
+          <div className={styles.formGroup}>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default ProfileForm
+export default ProfileForm;
