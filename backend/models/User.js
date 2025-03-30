780f891f277e9179b0171ce9a4d7beb5cd2fd107
annuu1 <mannusng876@gmail.com>
2025-03-31 03:25:01 +0530

    updated user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  firstName:{type:String, required:true},
-  lastName:{type:String, required:true},
-  // username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, unique: true, lowercase: true, trim: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, unique: true, lowercase: true, trim: true, required: true },
+  username: { type: String, unique: true, trim: true, sparse: true },
   password: { type: String, required: true },
+  preferences: { type: String, default: '' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
